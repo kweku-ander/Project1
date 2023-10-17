70fee6c3c91f794cfa30bdcb968980bdb8f505e0
kweku-ander <alexanderdarkoh8@gmail.com>
2023-10-17 16:01:51 +0000

    new changes on likes (controllers and routes)

---
+++ controllers/likesController.js
@@ -0,0 +1,46 @@
+import likesModel from "../models/likesModel.js";
+
+// Getting Likes
+const getPostLike = async (req, res) => {
+    try {
+        const addNewlike = req.body.id;
+        if (addNewlike) {
+            const newLike = await likesModel.create(addNewlike);
+            return res.status(201).json({ message: "successful..." })
+        }
+
+    } catch (error) {
+        return res.status(500).json({ message: "failed", error })
+    }
+}
+
+
+// Getting All Likes
+const getAllLikes = async (req, res) => {
+    try {
+        const likes = await likesModel.find({ where: { id } })
+        if (likes) {
+            return res.status(200).json({ message: "Successful" });
+        }
+    } catch (error) {
+        return res.status(500).json({ message: "Failed...", error })
+    }
+}
+
+// Deleting
+const deleteLike = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deletedPostLike = await likesModel.destroy(id);
+        return res.status(200).json({ message: "Deleted successfully" });
+    } catch (error) {
+        return res.status(500).json({ massage: "Error Unliking", error })
+    }
+};
+
+
+
+
+
+
+export default { getPostLike, getAllLikes, deleteLike };
