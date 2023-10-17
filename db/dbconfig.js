ad17c576fe67bcb08daa4112d127afe3cba3cf45
kweku-ander <58990881+kweku-ander@users.noreply.github.com>
2023-10-17 17:10:01 +0000

    Update dbconfig.js

--- db/dbconfig.js
+++ db/dbconfig.js
@@ -1,10 +1,1 @@
-import Sequelize from "sequelize";
 
-const sequelize = new Sequelize('mini_project', 'root', '',{
-host: "localhost",
-dialect: "mysql",
-logging: false
-
-})
-
-export default sequelize;
